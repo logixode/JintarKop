296b58d3c819476a6d271a0f1b64d8840e1391ef
logixode <rohmad1230@gmail.com>
2020-07-15 03:41:17 +0700

    add vuex state, mutation, and action

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,62 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import firebase from 'firebase/app';
+import "firebase/auth";
+import router from '@/router';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    user: null,
+    error: null
+  },
+  mutations: {
+    setUser(state, payload) {
+      state.user = payload;
+    },
+    setError(state, payload) {
+      state.error = payload;
+    },
+  },
+  actions: {
+    userSignIn({ commit }, payload) {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(payload.email, payload.password)
+        .then(
+          firebaseUser => {
+            commit('setUser', { email: firebaseUser.user.email });
+            commit('setError', null);
+            router.push('/');
+          },
+          (this.error = ""))
+        .catch(err => {
+          commit('setError', err.message);
+          // console.log(err);
+        });
+    },
+    userSignUp({ commit }, payload) {
+      commit('setLoading', true);
+      firebase.auth().createUserWithEmailAndPassword(payload.email, payload.password)
+        .then(firebaseUser => {
+          commit('setUser', { email: firebaseUser.user.email });
+          commit('setLoading', false);
+          router.push('/home');
+        })
+        .catch(error => {
+          commit('setError', error.message);
+          commit('setLoading', false);
+        });
+    },
+    autoSignIn({ commit }, payload) {
+      commit('setUser', { email: payload.email });
+    },
+    userSignOut({ commit }) {
+      firebase.auth().signOut();
+      commit('setUser', null);
+      router.push('/');
+    }
+  },
   modules: {}
 });
