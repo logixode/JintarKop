e22e4915b571eaf9dd1a17b3b980f8615d4951c9
logixode <rohmad1230@gmail.com>
2020-07-16 17:22:35 +0700

    changing theme color and default name

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  transpileDependencies: ["vuetify"]
+  transpileDependencies: ["vuetify"],
+  pwa: {
+    name: 'JintarKop',
+    themeColor: '#fff',
+    msTileColor: '#FFA71F',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'white',
+  }
 };
