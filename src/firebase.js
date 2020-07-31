2ef8aa7196429d735221ba3f23c9a3bd6e882951
logixode <rohmad1230@gmail.com>
2020-07-31 17:31:37 +0700

    reconstruction the firebase

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import firebase from "firebase/app";
+import "firebase/database";
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyBfSC9AkeIAiUpusRVJ49hMAX5U18xA8sk",
+  authDomain: "jintarkop-2b9d2.firebaseapp.com",
+  databaseURL: "https://jintarkop-2b9d2.firebaseio.com",
+  projectId: "jintarkop-2b9d2",
+  storageBucket: "jintarkop-2b9d2.appspot.com",
+  messagingSenderId: "602636053987",
+  appId: "1:602636053987:web:c39f1527c1dd52dcea04d6"
+};
+// Initialize Firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+export const db = firebase.database();
+export const hardwareDataRef = db.ref('dataHardware');
+export const notificationRef = db.ref('notifikasi');
+export default firebaseApp;
--- src/main.js
+++ src/main.js
@@ -4,8 +4,9 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 import axios from "axios";
-import firebase from "firebase/app";
 import vuetify from "./plugins/vuetify";
+import './firebase';
+import firebase from 'firebase/app';
 
 Vue.mixin(
   {
@@ -54,19 +55,6 @@ Vue.mixin(
 );
 Vue.prototype.$axios = axios;
 Vue.config.productionTip = false;
-
-// Your web app's Firebase configuration
-var firebaseConfig = {
-  apiKey: "AIzaSyBfSC9AkeIAiUpusRVJ49hMAX5U18xA8sk",
-  authDomain: "jintarkop-2b9d2.firebaseapp.com",
-  databaseURL: "https://jintarkop-2b9d2.firebaseio.com",
-  projectId: "jintarkop-2b9d2",
-  storageBucket: "jintarkop-2b9d2.appspot.com",
-  messagingSenderId: "602636053987",
-  appId: "1:602636053987:web:c39f1527c1dd52dcea04d6"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
 
 firebase.auth()
   .onAuthStateChanged((firebaseUser) => {
