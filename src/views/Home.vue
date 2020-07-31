1dacdb141b55c505adedfc09a2904d978c820dd9
logixode <rohmad1230@gmail.com>
2020-07-31 18:43:20 +0700

    adding status and type of coffe

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,10 +14,10 @@
         <div class="text-center">
           <Progress
             :transitionDuration="1450"
-            :strokeColor="value < 75 ? progressColor.blue : progressColor.brown"
+            :strokeColor="tingkatKekeringan < 75 ? progressColor.blue : progressColor.brown"
             :radius="75"
             :strokeWidth="10"
-            :value="value"
+            :value="tingkatKekeringan"
             class="main-progress"
           >
             <template v-slot:footer>
@@ -29,38 +29,42 @@
       <v-col cols="6" class="text-center">
         <Progress
           :transitionDuration="1450"
-          :strokeColor="(100 - value) >= 75 ? progressColor.blue : progressColor.brown"
+          :strokeColor="kadarAir >= 25 ? progressColor.blue : progressColor.brown"
           :radius="45"
           :strokeWidth="8"
-          :value="100 - value"
+          :value="kadarAir"
         >
           <template v-slot:footer>
             <b>Kadar Air</b>
           </template>
         </Progress>
-
-        <v-btn
-          v-if="isClosed"
-          outlined
-          large
-          block
-          color="orange"
-          class="mt-5"
-          @click="openToolCover"
-        >Tutup Alat</v-btn>
-        <v-btn
-          v-else
-          large
-          outlined
-          block
-          color="orange"
-          class="p-2 mt-5"
-          @click="openToolCover"
-        >Buka Alat</v-btn>
+        <div class="row ml-1 mt-3">
+          <h4>Jenis Kopi :</h4>
+          <div class="ml-1">
+            <p>{{ jenisKopi }}</p>
+          </div>
+        </div>
+        <div class="row ml-1 mt-1">
+          <h4>Status :</h4>
+          <div class="ml-1">
+            <p v-if="tingkatKekeringan <= 65">Masih basah</p>
+            <p v-else-if="tingkatKekeringan <= 92">Hampir Kering</p>
+            <p v-else>Sudah Kering</p>
+          </div>
+        </div>
       </v-col>
     </v-row>
 
-    <v-btn block large outlined color="brown" class="mt-5" to="/setting">Atur alat</v-btn>
+    <v-btn
+      v-if="isClosed"
+      outlined
+      block
+      color="orange"
+      class="mt-5"
+      @click="openToolCover"
+    >Tutup Alat</v-btn>
+    <v-btn v-else outlined block color="orange" class="p-2 mt-5" @click="openToolCover">Buka Alat</v-btn>
+    <v-btn block outlined color="brown" class="mt-5" to="/setting">Atur alat</v-btn>
 
     <v-card color="white" class="px-3 mt-5 pt-5 pb-3 text-center">
       <h3>Data Pengering</h3>
@@ -107,15 +111,21 @@ export default {
     isClosed: false
   }),
   computed: {
-    value() {
+    tingkatKekeringan() {
       const percentage = (this.dataBerat.kering / this.dataBerat.basah) * 100;
       return percentage.toFixed(2);
+    },
+    kadarAir() {
+      return (100 - this.tingkatKekeringan).toFixed(2);
     },
     suhuKelembapan() {
       return this.$store.state.hardwareData.suhuKelembapan;
     },
     dataBerat() {
       return this.$store.state.hardwareData.berat;
+    },
+    jenisKopi() {
+      return this.$store.state.hardwareData.jenisKopi;
     }
   },
   methods: {
@@ -139,10 +149,10 @@ p {
 </style>
 <style>
 .main-progress .percent__int {
-  font-size: 45px;
+  font-size: 45px !important;
 }
 .main-progress .percent__dec,
 .main-progress .percent_sign {
-  font-size: 22px;
+  font-size: 22px !important;
 }
 </style>
