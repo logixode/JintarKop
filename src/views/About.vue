9c52fa6df0653422dc1d9181cd4cdc6e2b043b84
logixode <rohmad1230@gmail.com>
2020-08-08 17:59:33 +0700

    making layout

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,45 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-container class="px-5">
+    <div class="text-center mb-2">
+      <img alt="Jintarkop" class="main-logo mb-3" src="../assets/logo.png" />
+    </div>
+    <v-card color="white" class="px-4 pt-4 pb-3">
+      <p class="mb-3">
+        <span class="font-weight-bold orange--text">Jintarkop</span> (jemuran pintar kopi) adalah suatu aplikasi yang digunakan untuk memonitoring kadar air pada kopi dengan menggunakan sensor timbangan pada alat yang dibuat.
+      </p>
+      <h4 class="orange--text mt-5">Pembimbing :</h4>
+      <p class="ml-2">Endi Sailul Haq, S.t., M.Kom.</p>
+      <h4 class="orange--text mt-5">Anggota Tim :</h4>
+      <ol>
+        <li>Erisa Zahra Dina (Ketua)</li>
+        <li>Faida Evita Maynasari</li>
+        <li>Zulfa Aulia</li>
+        <li>Adining Tri Karunia Putri</li>
+        <li>Gading Bengawan</li>
+        <li>Agung Wahyu Widodo</li>
+        <li>Dika Hermawan</li>
+        <li>Rohmad Kurniadi</li>
+        <li>Feti Firdaus</li>
+        <li>Muhammad Rizki Ismail</li>
+      </ol>
+    </v-card>
+  </v-container>
 </template>
+
+<script >
+export default {
+  data: () => ({})
+};
+</script>
+
+<style scoped>
+p {
+  margin-bottom: 0;
+}
+.main-logo {
+  height: 120px;
+}
+.v-card p {
+  text-align: justify;
+}
+</style>
